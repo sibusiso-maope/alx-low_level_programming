c628123edca5ab754b4741f9be40f16ecd9da3b7
sibusiso-maope <sibusiso.maope@gmail.com>
2023-08-10 12:04:27 -0700

    0x00 hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * My first c code 
+ *
+ * These comments are going to be ignored by the compiler
+ * 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * My first c code 
+ *
+ * These comments are going to be ignored by the compiler
+ * 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
