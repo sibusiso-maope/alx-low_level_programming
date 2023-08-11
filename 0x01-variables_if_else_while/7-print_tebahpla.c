95f7246b5aa91f0e9df0ac02690ab9eb8ebc215f
sibusiso-maope <sibusiso.maope@gmail.com>
2023-08-11 09:33:44 -0700

    My 8th task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,13 +3,13 @@
 /**
  * main - main function
  *
- * Return: Always 0
+ * Return: always 0
  */
 
 int main(void)
 {
 	char i;
-	
+
 	for (i = 'z' ; i >= 'a' ; i--)
 		putchar(i);
 	putchar('\n');
