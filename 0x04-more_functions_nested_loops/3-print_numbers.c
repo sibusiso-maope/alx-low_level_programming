d0e68bba55e67cdb1ceda32b2f1e08be3e9aecfe
sibusiso-maope <sibusiso.maope@gmail.com>
2023-08-17 08:19:18 -0700

    My Print numbers task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * print_numbers - print numbers from 0 to 9
@@ -9,6 +10,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i < 10 ; i++)
+	{
 		_putchar(i + '0');
+	}
 	_putchar('\n');
+}
