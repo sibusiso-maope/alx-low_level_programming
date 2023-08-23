61b0aeb5549a513cc73017161a810ad3ed8d870c
sibusiso-maope <sibusiso.maope@gmail.com>
2023-08-23 09:36:36 -0700

    Strcat Task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int k;
+	int l;
+
+	k = 0;
+	while (dest[k] != '\0')
+	{
+		k++;
+	}
+	l = 0;
+	while (src[l] != '\0')
+	{
+		dest[l] = src[l];
+		k++;
+		l++;
+	}
+
+	dest[k] = '\0';
+	return (dest);
+}
