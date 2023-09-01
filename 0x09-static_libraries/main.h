05e28b935d7895739b389cc7eb9a9da0b6426bd2
sibusiso-maope <sibusiso.maope@gmail.com>
2023-09-01 11:15:55 -0700

    Fixed error

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <string.h>
-                    	
-int _putchar(char c);         	
+
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
