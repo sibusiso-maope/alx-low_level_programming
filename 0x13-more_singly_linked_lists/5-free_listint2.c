303c7876864a1e68d46b5c44aa3d0c0a2fcb969b
sibusiso-maope <Sibusiso.Maope@gmail.com>
2023-09-26 10:58:23 -0700

    My free list task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+	*head = NULL;
+}
