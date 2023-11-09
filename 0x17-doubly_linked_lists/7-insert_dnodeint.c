efc2eb177a57aa3e728e4fb430333395be003648
sibusiso-maope <sibusiso.maope@gmail.com>
2023-11-09 15:35:05 -0800

    My Insert node task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		current = *h;
 		k = 1;
-		
+
 		if (current != NULL)
 			while (current->prev != NULL)
 				current = current->prev;
