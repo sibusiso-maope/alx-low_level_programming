05eee160f92f675e6332aaec789d1dab865c764a
sibusiso-maope <sibusiso.maope@gmail.com>
2023-11-30 23:44:53 -0800

    My task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,7 +17,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	lead = key_lead((const unsigned char *)key, ht->size);
+	lead = key_index((const unsigned char *)key, ht->size);
 	if (lead >= ht->size)
 		return (NULL);
 
