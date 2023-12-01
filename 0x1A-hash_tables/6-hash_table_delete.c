b9c2ea3769027a780f890049cfcf6374fb2c179b
sibusiso-maope <sibusiso.maope@gmail.com>
2023-12-01 00:22:51 -0800

    My Task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int k;
+
+	for (k = 0; k < ht->size; k++)
+	{
+		if (ht->array[k] != NULL)
+		{
+			node = ht->array[k];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
