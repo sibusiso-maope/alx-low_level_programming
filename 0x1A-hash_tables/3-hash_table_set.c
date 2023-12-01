a683620f555ccd67898fa0a725d75d92f5692ff9
sibusiso-maope <sibusiso.maope@gmail.com>
2023-11-30 23:23:36 -0800

    My task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	for (k = index; ht->array[k]; k++)
 	{
 		if (strcmp(ht->array[k]->key, key) == 0)
@@ -33,23 +32,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-
 	new = malloc(sizeof(hash_node_t));
-
 	if (new == NULL)
 	{
 		free(value_copy);
 		return (0);
 	}
-
 	new->key = strdup(key);
-
 	if (new->key == NULL)
 	{
 		free(new);
 		return (0);
 	}
-
 	new->value = value_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
